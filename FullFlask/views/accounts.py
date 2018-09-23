1af3ae8994f33375054859ce83eb80e5128c6ad4
tqtl911@163.com <19930911cXS>
2018-09-23 16:29:26 +0800

    通过SQLAlchemy查询数据

--- FullFlask/views/accounts.py
+++ FullFlask/views/accounts.py
@@ -9,10 +9,21 @@
 
 
 from flask import blueprints
+from FullFlask import models
 
 ac = blueprints.Blueprint('ac', __name__)
 
 
 @ac.route('/login', methods=['GET', 'POST'])
 def login():
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    engine = create_engine("mysql+pymysql://root:Tqtl911!@%*)@mysql.cuixiaozhao.com:3306/FullFlask?charset=utf8")
+    maker = sessionmaker(bind=engine)
+    session = maker()
+    result = session.query(models.Users).all()
+    session.close()
+    print(
+        result)  # [<FullFlask.models.Users object at 0x106123c88>, <FullFlask.models.Users object at 0x106123dd8>, <FullFlask.models.Users object at 0x106123a90>, <FullFlask.models.Users object at 0x1061239e8>]
+
     return 'Login it.'
