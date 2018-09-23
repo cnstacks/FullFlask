a61a71afe1b3f5806af72a46e3892ff654b71a61
tqtl911@163.com <19930911cXS>
2018-09-23 17:28:58 +0800

    flask中的离线脚本应用！

---
+++ drop_table.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# Project: FullFlask 
+# Software: PyCharm
+# Time    : 2018-09-23 17:08
+# File    : drop_table.py
+# Author  : 天晴天朗
+# Email   : tqtl@tqtl.org
+""
+"""
+1、Web运行时候，flask程序运行起来，用户通过浏览器访问；
+2、离线脚本，即自定义的一个py文件+使用flask中定义好的功能；
+"""
+from FullFlask import db
+from FullFlask import create_app
+
+app = create_app()
+with app.app_context():
+    # db.drop_all()
+    db.create_all()
